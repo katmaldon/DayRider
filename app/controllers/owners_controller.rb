17d04d392597e505c48e31eb4f78deff0dc1a3f8
github username <github email address>
2020-05-06 10:21:44 -0400

    owners index and show done

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -7,7 +7,8 @@ class OwnersController < ApplicationController
 
     def show
       @owner = Owner.find(params[:id])
-
     end
+
+
 
 end
--- app/models/owner.rb
+++ app/models/owner.rb
@@ -2,4 +2,7 @@ class Owner < ApplicationRecord
 
   has_many :bikes
 
+  # validates: :name, presence: true
+  # validates: :age, numerality: [greater_than 17]
+
 end
