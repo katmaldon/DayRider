244872d2c4d8faa2a9bb463e0031bc0be61cc014
github username <github email address>
2020-05-07 05:11:48 -0400

    views uniformity and cohesion, forms

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -3,11 +3,10 @@ class RentalsController < ApplicationController
     before_action :find_rental, only: [:show, :edit, :update, :destroy]
 
     def index
-
+        redirect_to bikes_path #placeholder to avoid routing errors
     end
 
     def show
-
     end
 
     def new
@@ -16,7 +15,7 @@ class RentalsController < ApplicationController
         @renters = Renter.all
         @bikes = Bike.available_bikes
     end
-    
+
     def create
         bike_id_value = session[:bike]["id"]
         # renter_id_value = session[:renter]["id"]
@@ -24,30 +23,29 @@ class RentalsController < ApplicationController
         @rental = Rental.create(renter_id:20, bike_id: bike_id_value)
         redirect_to rental_path(@rental.id)
     end
-    
-    
+
     def edit
-        
     end
-    
+
     def update
         @rental.update(rental_params(:comment, :rating))
         redirect_to rental_path(@rental)
-        
     end
-    
+
     def destroy
         @rental.destroy
         redirect_to new_rental_path
     end
-    
+
+
     private
+
     def rental_params(*args)
         params.require(:rental).permit(*args)
     end
-    
+
     def find_rental
         @rental = Rental.find(params[:id])
     end
-    
+
 end
