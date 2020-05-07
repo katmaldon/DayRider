2401d891a0c900b2231875d1e77ac47ae8f50766
github username <github email address>
2020-05-07 03:15:29 -0400

    renter views and contrll

--- app/controllers/renters_controller.rb
+++ app/controllers/renters_controller.rb
@@ -1,19 +1,21 @@
 class RentersController < ApplicationController
-    
+
     def index
+        @renters = Renter.all
     end
-    
+
     def show
         @renter = Renter.find(params[:id])
     end
 
     def new
         @renter = Renter.new
+        @shops = Shop.all
     end
 
     def create
-        renter = Renter.create(renter_params(:name, :age, :location))
-        redirect_to renter_path(renter)
+        @renter = Renter.create(renter_params(:name, :age, :location))
+        redirect_to renter_path(@renter)
     end
 
     private
