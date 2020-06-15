ed3bb26b7c868083dd8f8736d8c6d02a72dbe132
github username <36998937+katmaldon@users.noreply.github.com>
2020-06-15 13:59:11 -0400

    updated gemfile

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_07_212515) do
+ActiveRecord::Schema.define(version: 2020_05_08_204537) do
 
   create_table "bikes", force: :cascade do |t|
     t.integer "owner_id"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2020_05_07_212515) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "bike_img"
+    t.integer "price"
   end
 
   create_table "owners", force: :cascade do |t|
